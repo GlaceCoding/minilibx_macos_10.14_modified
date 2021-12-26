bfc49032d62a53d0c6c038de437013aa4e31c4c6
Guillaume <92152391+GlaceCoding@users.noreply.github.com>
2021-12-26 17:47:06 +0100

    Fix syntax

--- mlx.h
+++ mlx.h
@@ -12,7 +12,6 @@
 **   MinilibX -  Please report bugs
 */
 
-
 /*
 ** FR msg - FR msg - FR msg
 **
@@ -41,102 +40,91 @@
 ** c'est normalement le cas.
 */
 
-
 #ifndef MLX_H
+# define MLX_H
 
-#define	MLX_H
-
-
-void	*mlx_init();
+void			*mlx_init(void);
 /*
 **  needed before everything else.
 **  return (void *)0 if failed
 */
 
-
 /*
 ** Basic actions
 */
 
-void	*mlx_new_window(void *mlx_ptr, int size_x, int size_y, char *title);
+void			*mlx_new_window(void *mlx_ptr, int size_x, int size_y,
+					char *title);
 /*
 **  return void *0 if failed
 */
-int	mlx_clear_window(void *mlx_ptr, void *win_ptr);
-int	mlx_pixel_put(void *mlx_ptr, void *win_ptr, int x, int y, int color);
+int				mlx_clear_window(void *mlx_ptr, void *win_ptr);
+int				mlx_pixel_put(void *mlx_ptr, void *win_ptr, int x, int y,
+					int color);
 /*
 **  origin for x & y is top left corner of the window
 **  y down is positive
 **  color is 0x00RRGGBB
 */
 
-
 /*
 ** Image stuff
 */
 
-void	*mlx_new_image(void *mlx_ptr,int width,int height);
+void			*mlx_new_image(void *mlx_ptr, int width, int height);
 /*
 **  return void *0 if failed
 */
-char	*mlx_get_data_addr(void *img_ptr, int *bits_per_pixel,
-			   int *size_line, int *endian);
+char			*mlx_get_data_addr(void *img_ptr, int *bits_per_pixel,
+					int *size_line, int *endian);
+
 /*
 **  endian : 0 = sever X is little endian, 1 = big endian
-**  endian : useless on macos, client and graphical framework have the same endian
+**  endian : useless on macos, client and graphical framework have the same
+**           endian
 */
-int	mlx_put_image_to_window(void *mlx_ptr, void *win_ptr, void *img_ptr,
-				int x, int y);
-unsigned int	mlx_get_color_value(void *mlx_ptr, int color);
 
+int				mlx_put_image_to_window(void *mlx_ptr, void *win_ptr,
+					void *img_ptr, int x, int y);
+unsigned int	mlx_get_color_value(void *mlx_ptr, int color);
 
 /*
 ** dealing with Events
 */
 
-int	mlx_mouse_hook (void *win_ptr, int (*funct_ptr)(), void *param);
-int	mlx_key_hook (void *win_ptr, int (*funct_ptr)(), void *param);
-int	mlx_expose_hook (void *win_ptr, int (*funct_ptr)(), void *param);
-
-int	mlx_loop_hook (void *mlx_ptr, int (*funct_ptr)(), void *param);
-int	mlx_loop (void *mlx_ptr);
-
-
-/*
-**  hook funct are called as follow :
-**
-**   expose_hook(void *param);
-**   key_hook(int keycode, void *param);
-**   mouse_hook(int button, int x,int y, void *param);
-**   loop_hook(void *param);
-**
-*/
-
-
-/*
-**  Usually asked...
-*/
-
-int	mlx_string_put(void *mlx_ptr, void *win_ptr, int x, int y, int color,
-		       char *string);
-void	*mlx_xpm_to_image(void *mlx_ptr, char **xpm_data,
-			  int *width, int *height);
-void	*mlx_xpm_file_to_image(void *mlx_ptr, char *filename,
-			       int *width, int *height);
-int	mlx_destroy_window(void *mlx_ptr, void *win_ptr);
-
-int	mlx_destroy_image(void *mlx_ptr, void *img_ptr);
+int				mlx_mouse_hook(void *win_ptr, int (*funct_ptr)(), void *param);
+				//		int (*mouse_hook)(int button, int x, int y, void *param)
+int				mlx_key_hook(void *win_ptr, int (*funct_ptr)(), void *param);
+				//		int (*key_hook)(int keycode, void *param)
+int				mlx_expose_hook(void *win_ptr, int (*funct_ptr)(), void *param);
+				// 							   int (*funct_ptr)(void *param)
+int				mlx_loop_hook(void *mlx_ptr, int (*funct_ptr)(), void *param);
+				// 							 int (*funct_ptr)(void *param)
+int				mlx_loop(void *mlx_ptr);
 
 /*
 **  generic hook system for all events, and minilibX functions that
 **    can be hooked. Some macro and defines from X11/X.h are needed here.
 */
 
-int	mlx_hook(void *win_ptr, int x_event, int x_mask,
-                 int (*funct)(), void *param);
+int				mlx_hook(void *win_ptr, int x_event, int x_mask, int (*funct)(), void *param);
 
-int	mlx_do_key_autorepeatoff(void *mlx_ptr);
-int	mlx_do_key_autorepeaton(void *mlx_ptr);
-int	mlx_do_sync(void *mlx_ptr);
+int				mlx_do_key_autorepeatoff(void *mlx_ptr);
+int				mlx_do_key_autorepeaton(void *mlx_ptr);
+int				mlx_do_sync(void *mlx_ptr);
+
+/*
+**  Utils…
+*/
+
+int				mlx_string_put(void *mlx_ptr, void *win_ptr, int x, int y,
+					int color, char *string);
+void			*mlx_xpm_to_image(void *mlx_ptr, char **xpm_data,
+					int *width, int *height);
+void			*mlx_xpm_file_to_image(void *mlx_ptr, char *filename,
+					int *width, int *height);
+int				mlx_destroy_window(void *mlx_ptr, void *win_ptr);
+
+int				mlx_destroy_image(void *mlx_ptr, void *img_ptr);
 
 #endif /* MLX_H */
